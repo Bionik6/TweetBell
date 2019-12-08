748cfad4761166ac23baf324b495247df9364d45
Ibrahima Ciss <bionik6@gmail.com>
2019-12-08 21:36:12 +0000

    Refactor Tweet

--- TweetBell/Core/Entities/Tweet.swift
+++ TweetBell/Core/Entities/Tweet.swift
@@ -4,25 +4,12 @@ import CoreLocation
 struct Tweet {
   var id: Int
   var text: String
-//  var date: Date
   var user: User
   var coordinates: String?
-  
-  
-  var location: CLLocationCoordinate2D? {
-    return nil
-  }
-  
   
   init(json: JSON) {
     self.id = json["id"].intValue
     self.text = json["text"].stringValue
-//    self.date = DateFormatter().date(from: json["created_at"].stringValue)
     self.user = User(json: json["user"])
-    if let coordinates = json["geo"]["coordinates"].array {
-      // self.coordinates =
-      log.debug(coordinates)
-    }
- 
   }
 }
