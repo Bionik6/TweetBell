466827a37f41af47f3f2bd2c6abfa766b56fb419
Ibrahima Ciss <bionik6@gmail.com>
2019-12-16 08:30:00 +0000

    Update MapViewController

--- TweetBell/Presentation/User Interfaces/Map/MapViewController.swift
+++ TweetBell/Presentation/User Interfaces/Map/MapViewController.swift
@@ -6,8 +6,8 @@ typealias MapUserInterfaceView = MapUserInterface & UIView
 final class MapViewController: UIViewController, AlertShowable {
   
   private let viewModel: MapViewModel
-  private var disposeBag = Set<AnyCancellable>()
   private let userInterface: MapUserInterfaceView
+  private var subscriptions = Set<AnyCancellable>()
   
   init(viewModel: MapViewModel, userInterface: MapUserInterfaceView) {
     self.viewModel = viewModel
@@ -25,26 +25,21 @@ final class MapViewController: UIViewController, AlertShowable {
     setupObservers()
   }
   
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    view.accessibilityIgnoresInvertColors = false 
-  }
-  
   /// Wire observers to userinterface so the view can react when a publisher emits a signal
   private func setupObservers() {
     viewModel.$currentLocation.receive(on: DispatchQueue.main).sink { [weak self] location in
       guard let location = location else { return }
       self?.userInterface.recenter(at: location)
       self?.viewModel.getRecentTweets()
-    }.store(in: &disposeBag)
+    }.store(in: &subscriptions)
     
     viewModel.$tweets.receive(on: DispatchQueue.main).sink { [weak self] tweets in
       self?.userInterface.showTweetsOnMap(tweets: tweets)
-    }.store(in: &disposeBag)
+    }.store(in: &subscriptions)
     
     viewModel.$locationPermissionGiven.receive(on: DispatchQueue.main).sink { [weak self] permission in
       guard permission == false else { return }
       self?.showAlert(message: "Please verify your internet and make sure you give location permission")
-    }.store(in: &disposeBag)
+    }.store(in: &subscriptions)
   }
 }
